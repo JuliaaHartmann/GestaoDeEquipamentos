fed7ae23edae4a39f068c37fea46498e653f5880
Júlia Hartmann <juliahartmann@outlook.com.br>
2026-04-09 14:51:39 -0300

    Adicionado cadastro de Equipamentos

---
+++ GestaoDeEquipamentos.ConsoleApp/Equipamento.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace GestaoDeEquipamento.ConsoleApp;
+
+public class Equipamento
+{
+    public string id;
+    public string nome;
+    public string fabricante;
+    public decimal precoAquisicao;
+    public DateTime dataFabricacao;
+
+}
