f2160c44f08bdc28e50f9ff48349a4ac4997d2cb
Júlia Hartmann <juliahartmann@outlook.com.br>
2026-04-09 16:25:52 -0300

    Adicionado exclusão de equipamentos

--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -86,7 +86,7 @@ while (true)
         Console.WriteLine("---------------------------------");
         Console.WriteLine($"O registro \"{novoEquipamento.nome}\" foi cadastrado com sucesso!");
         Console.WriteLine("---------------------------------");
-        Console.WriteLine("Digite ENTER para continuar");
+        Console.Write("Digite ENTER para continuar");
         Console.ReadLine();
     }
 
@@ -150,7 +150,7 @@ while (true)
             Console.WriteLine("---------------------------------");
             Console.WriteLine($"Não foi possível encontrar o equipamento informado!");
             Console.WriteLine("---------------------------------");
-            Console.WriteLine("Digite ENTER para continuar");
+            Console.Write("Digite ENTER para continuar");
             Console.ReadLine();
             continue;
         }
@@ -197,14 +197,84 @@ while (true)
         Console.WriteLine("---------------------------------");
         Console.WriteLine($"O registro \"{idSelecionado}\" foi editado com sucesso!");
         Console.WriteLine("---------------------------------");
-        Console.WriteLine("Digite ENTER para continuar");
+        Console.Write("Digite ENTER para continuar");
         Console.ReadLine();
 
     }
 
     else if (opcaoMenu == "3")
     {
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("Gestão de Equipamentos");
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("Exclusão de Equipamento");
+        Console.WriteLine("---------------------------------");
 
+        Console.WriteLine(
+            "{0, -7} | {1, -15} | {2, -15} | {3, -22} | {4, -10}",
+            "Id", "Nome", "Fabricante", "Preço de Aquisição", "Data de Fabricação"
+        );
+
+        for (int i = 0; i < equipamentos.Length; i++)
+        {
+            Equipamento? e = equipamentos[i];
+
+            if (e == null)
+                continue;
+
+            Console.WriteLine(
+                "{0, -7} | {1, -15} | {2, -15} | {3, -22} | {4, -10}",
+                e.id, e.nome, e.fabricante, e.precoAquisicao.ToString("C2"), e.dataFabricacao.ToShortDateString()
+            );
+        }
+
+        Console.WriteLine("---------------------------------");
+        string? idSelecionado;
+
+        do
+        {
+            Console.WriteLine("Digite o ID do equipamento que deseja excluir: ");
+            idSelecionado = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(idSelecionado) && idSelecionado.Length == 7)
+                break;
+        } while (true);
+
+        bool equipamentoExcluido = false;
+
+        for (int i = 0; i < equipamentos.Length; i++)
+        {
+            Equipamento? e = equipamentos[i];
+
+            if (e == null)
+                continue;
+
+            if (e.id == idSelecionado)
+            {
+                equipamentos[i] = null;
+                equipamentoExcluido = true;
+                break;
+            }
+
+
+        }
+        if (equipamentoExcluido)
+        {
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine($"O registro \"{idSelecionado}\" foi excluído com sucesso!");
+            Console.WriteLine("---------------------------------");
+            Console.Write("Digite ENTER para continuar");
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine($"Não foi possível encontrar o registro \"{idSelecionado}\".");
+            Console.WriteLine("---------------------------------");
+        }
+
+        Console.Write("Digite ENTER para continuar");
+        Console.ReadLine();
     }
 
     else if (opcaoMenu == "4")
